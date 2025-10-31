aabd5a0f2cf407905915c125c190f3ad4e358076
akshaykumaraddanki08-boop <akshaykumaraddanki08@gmail.com>
2025-10-31 09:54:14 +0530

    Create bankaccount.java

---
+++ bankaccount.java
@@ -0,0 +1,51 @@
+package src;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid or insufficient funds for withdrawal!");
+        }
+    }
+
+    public void displayAccountDetails() {
+        System.out.println("\n--- Account Details ---");
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Current Balance: $" + balance);
+        System.out.println("-----------------------\n");
+    }
+}
