114235965b87ac0fbba8960638cae909c3ff79fc
akshaykumaraddanki08-boop <akshaykumaraddanki08@gmail.com>
2025-10-31 09:57:36 +0530

    Create complie.java

---
+++ complie.java
@@ -0,0 +1,13 @@
+compile the program:
+javac src/*.java
+run the program:
+java src.Main
+Future Improvements
+
+Add login authentication
+
+Save data to a file or database
+
+Add transaction history
+
+Add GUI (JavaFX or Swing)
